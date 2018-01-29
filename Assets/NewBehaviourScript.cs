032c7a5555ec0dc60f53940f93a6d524eaac3226
kobake <pg.kobake@gmail.com>
2018-01-30 02:18:17 +0900

    sample

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Dynamic;
+using Newtonsoft.Json;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        string jsontext = "{'hello':'world'}";
+        var jsonobj = JsonConvert.DeserializeObject<dynamic>(jsontext);
+        string v = jsonobj.hello;
+        Debug.Log("hello value is " + v);
+        
+    }
+    
+    // Update is called once per frame
+    void Update () {
+    
+    }
+}
